0ed5f0e55e8a19cc8124080ecb44de220fe440e2
zaitun <3autyh@gmail.com>
2023-01-25 18:34:35 +0300

    Some refactoring, need to add db, 1 and 2 steps almost done

--- keybords.py
+++ keybords.py
@@ -1,35 +1,81 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+    InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, ReplyKeyboardRemove
+
 webapp = WebAppInfo(url="https://eclectic-melba-d24886.netlify.app/")
-hockey_teams = ["Ак барс", "Чеза", "Не шарю за хоккей", "но тут еще", 'Много команд']
-inline_hockey = InlineKeyboardButton('Хоккей', callback_data='hockey')
-inline_football = InlineKeyboardButton('Футбол', callback_data='football')
-inline_basketball = InlineKeyboardButton('Баскетбол', callback_data='basketball')
+
+categories = ["Футбол", "Хоккей", "Баскетбол", "Шахматы", "Волейбол", "Бейсбол", "Гольф", "Теннис", "Спортивная мафия",
+              "КС ГО", "Блогеры", "Настольный теннис", "Швеи", "клуб математиков"]
+hockey_teams = ["Ак барс", "Чеза", "Салкын Чай", "Рубин", 'Спартак', 'Москва', 'Зенит', 'Ливерпуль', 'Ювентус',
+                'Юрматы', 'Салават Юлаев', 'Манчестер', 'Бишекташ', 'Финербахче']
+
+
 inline_base = InlineKeyboardButton('Базовый', callback_data='base')
 inline_stand = InlineKeyboardButton('Стандарт', callback_data='standard')
 inline_prem = InlineKeyboardButton('Премиум', callback_data='premium')
 inl_get_sub = InlineKeyboardButton('Стать членом сообщества', callback_data='buy_sub')
 inl_buy = InlineKeyboardButton('Оплатить', callback_data='buy')
-inl_store = InlineKeyboardButton(text="Store", web_app=webapp)
+inl_store = InlineKeyboardButton("Store", web_app=webapp)
 inl_menu = InlineKeyboardButton('Перейти в меню', callback_data='menu')
 inl_back = InlineKeyboardButton('Назад', callback_data='back')
-cancel_kb = KeyboardButton('/cancel')
+
 find_club = KeyboardButton('Найти клуб️')
 market = KeyboardButton('Маркетплейс️')
 
-inline_categories = InlineKeyboardMarkup().row(inline_hockey, inline_football, inline_basketball)
-cancel_button = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(cancel_kb)
+
+cancel_button = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton('/cancel'))
 back_btn = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton('Назад'))
 default_btn = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(find_club).add(market)
-club_btn = InlineKeyboardMarkup().add(inl_get_sub).add(inl_store).add(inl_menu)
+
+
+sub_default_btn = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(KeyboardButton("Мои подписки"),
+                                                                                        KeyboardButton("Мои бронирования"))\
+    .add(find_club, market).add(KeyboardButton("Помощь"))
+club_btn = InlineKeyboardMarkup().add(inl_get_sub).add(inl_store).add(inl_menu).add(inl_back)
 buy_btn = InlineKeyboardMarkup().add(inl_buy)
 
 subs_btn = InlineKeyboardMarkup().row(inline_base, inline_stand, inline_prem).add(inl_back)
-all_teams_btn = InlineKeyboardMarkup()
+categ_page = 0
+team_page = 0
+len_cat = len(categories)
+len_team = len(hockey_teams)
 
-for team in hockey_teams:
-    all_teams_btn.add(InlineKeyboardButton(f"{team}", callback_data=team))
-all_teams_btn.add(InlineKeyboardButton('Назад', callback_data='back'))
+
+def get_categories(str_):
+    all_categories_btn = InlineKeyboardMarkup(row_width=2)
+    global categ_page
+    if str_ == '<' and categ_page >= 10:
+        categ_page -= 10
+    elif str_ == '>':
+        categ_page += 10
+    for i in range(len_cat - categ_page):
+        all_categories_btn.row(InlineKeyboardButton(f"{categories[i + categ_page]}",
+                                                    callback_data=categories[i + categ_page]))
+        if i == 9:
+            break
+    if len(categories) > 10:
+        all_categories_btn.add(InlineKeyboardButton("<", callback_data="<"),
+                               InlineKeyboardButton(">", callback_data=">"))
+    return all_categories_btn
+
+
+def get_all_teams(str_):
+    all_teams_btn = InlineKeyboardMarkup()
+    global team_page
+    if str_ == '<' and team_page >= 10:
+        team_page -= 10
+    elif str_ == '>':
+        team_page += 10
+    for i in range(len_team - team_page):
+        all_teams_btn.add(InlineKeyboardButton(f"{hockey_teams[i + team_page]}",
+                                               callback_data=hockey_teams[i + team_page]))
+        if i == 9:
+            break
+    if len(hockey_teams) > 10:
+        all_teams_btn.add(InlineKeyboardButton('Назад', callback_data='back'), InlineKeyboardButton("<", callback_data="<"),
+                          InlineKeyboardButton(">", callback_data=">"))
+    else:
+        all_teams_btn.add(InlineKeyboardButton('Назад', callback_data='back'))
+    return all_teams_btn
 
 
 def get_web_app():
