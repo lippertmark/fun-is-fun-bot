070a4ef87a636a57fe4ace8169b068c4c121486b
zaitun <3autyh@gmail.com>
2023-02-28 20:04:39 +0300

    conflicts

--- load.py
+++ load.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 from sqlalchemy.engine import URL
 from db.base import new_async_engine, new_session_maker
 from db.FSMStorage import Storage
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
+
 TOKEN_TELEGRAM_CLIENT = os.getenv("TOKEN_TELEGRAM_CLIENT")
 PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN")
 
@@ -20,7 +20,6 @@ db_url = URL.create("postgresql+asyncpg",
                     database=os.getenv('DB_NAME'))
 engine = new_async_engine(db_url, echo=False)
 sm = new_session_maker(engine)
-
 
 storage = Storage("client", sm)
 bot = Bot(token=TOKEN_TELEGRAM_CLIENT, parse_mode="HTML")
